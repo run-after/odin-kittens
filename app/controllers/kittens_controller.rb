9968d37d20fe432b814a84d0357a06cef62c263f
run-after <run.after@protonmail.com>
2020-05-29 14:31:46 -0500

    Finish project

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -16,6 +16,10 @@ class KittensController < ApplicationController
 
   def index
     @kittens = Kitten.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens }
+    end
   end
 
   def edit
@@ -40,6 +44,10 @@ class KittensController < ApplicationController
 
   def show
     @kitten = Kitten.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @kitten }
+    end
   end
 
   private
